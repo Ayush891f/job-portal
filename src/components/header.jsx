474ae24555d0cba0eeaacd11939f6845adbe42c0
animated-pancake <acads108@gmail.com>
2025-01-15 09:30:33 +0530

    job page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link>
-        <img src="public\logo.png" className="h-20" alt="logo" />
+        <img src="/logo.png" className="h-20" alt="logo" />
         </Link>
 
       <div className="flex gap-8">
